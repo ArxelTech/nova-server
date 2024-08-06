ad56081e851b9b41dd87b702ece052683ab65693
NEON <danielemmanuel257@gmail.com>
2024-08-06 22:20:14 +0100

    websockets

--- src/gateways/rooms/rooms.gateway.ts
+++ src/gateways/rooms/rooms.gateway.ts
@@ -1,8 +1,25 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
 import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ transports: ['polling'], cors: '*' })
 export class RoomsGateway {
   @WebSocketServer()
   server: Server;
+
+  @SubscribeMessage('connected')
+  handleEvent(@MessageBody() data: unknown): WsResponse<unknown> {
+    this.server.emit('message', {
+      data: 'This is from the server',
+      msg: 'New message',
+    });
+    console.log(data);
+    const event = 'events';
+    return { event, data };
+  }
 }
